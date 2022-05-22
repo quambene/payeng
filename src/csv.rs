64ee3aee1f6310e136e737487131943e2cb13677
quambene <contact.quambene@gmail.com>
2022-05-22 15:18:04 +0200

    Refactor csv reader and writer

---
+++ src/csv.rs
@@ -0,0 +1,69 @@
+use crate::{
+    errors::FormatError,
+    models::{Account, CheckedTransaction, RawAccount, RawTransaction, Transaction},
+};
+use anyhow::Context;
+use std::collections::{hash_map::Entry, HashMap};
+
+pub fn read(csv_file: &str) -> Result<(Vec<u32>, HashMap<u32, Transaction>), anyhow::Error> {
+    // Prepare csv reader and remove/ignore all whitespaces
+    let mut csv_reader = csv::ReaderBuilder::new()
+        .trim(csv::Trim::All)
+        .from_path(csv_file)
+        .context(format!("Can't find csv file at path '{}'", csv_file))?;
+
+    // Collect time-ordered transaction ids in transaction_history; transactions have to be processed in chronological order
+    let mut transaction_history: Vec<u32> = vec![];
+
+    // The transaction events (dispute, resolve, chargeback) are aggregated into the transactions so that transaction_id is unique in the input data
+    let mut transactions: HashMap<u32, Transaction> = HashMap::new();
+
+    // Prepare all transactions for processing (read from file, conversion to business objects)
+    for record in csv_reader.deserialize() {
+        let raw_transaction: RawTransaction = record?;
+
+        // Check and verify input format via type CheckedTransaction
+        let checked_transaction: CheckedTransaction = raw_transaction.try_into()?;
+
+        match checked_transaction {
+            CheckedTransaction::Transaction(tx) => match transactions.entry(tx.transaction_id) {
+                Entry::Occupied(_) => {
+                    return Err(FormatError::UniqueTransactionId(tx.transaction_id))?;
+                }
+                Entry::Vacant(entry) => {
+                    transaction_history.push(tx.transaction_id);
+                    entry.insert(tx);
+                }
+            },
+            CheckedTransaction::TransactionEvent(event) => {
+                match transactions.get_mut(&event.transaction_id) {
+                    Some(transaction) => {
+                        if transaction.client_id == event.client_id {
+                            transaction.events.push(event.event_type)
+                        } else {
+                            // Assumption: client_id and transaction_id of the transaction event have to coincide with the actual transaction; ignore if this is not the case
+                            continue;
+                        }
+                    }
+                    // Assumption: transaction events which do not reference a transaction id can be ignored
+                    None => continue,
+                }
+            }
+        };
+    }
+
+    return Ok((transaction_history, transactions));
+}
+
+pub fn write(accounts: HashMap<u16, Account>) -> Result<(), anyhow::Error> {
+    // Prepare csv writer and configure to write csv records to stdout
+    let mut csv_writer = csv::Writer::from_writer(std::io::stdout());
+
+    // Convert all business objects (Account) to the output format (RawAccount) and write to stdout in csv format
+    for (_client_id, account) in accounts {
+        let raw_account: RawAccount = account.into();
+        csv_writer.serialize(raw_account)?;
+    }
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,85 @@
+mod csv;
 mod errors;
 mod models;
 mod payment_engine;
 
-use crate::{
-    errors::FormatError,
-    models::{CheckedTransaction, RawAccount, RawTransaction, Transaction},
-};
-use std::{
-    collections::{hash_map::Entry, HashMap},
-    env, io,
-};
-
-// TODO: Write test for different input files (with and without spaces)
+use anyhow::anyhow;
+use std::env;
 
 // Output is parsed to stdout
 // Errors are parsed to stderr via anyhow
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<(), anyhow::Error> {
     // Parse the command line arguments; the first argument (index 1) is the path to the input csv file
     let args: Vec<String> = env::args().collect();
-    let csv_file = &args[1];
 
-    // Prepare csv reader and remove/ignore all whitespaces
-    let mut csv_reader = csv::ReaderBuilder::new()
-        .trim(csv::Trim::All)
-        .from_path(csv_file)?;
+    let csv_file = if args.len() > 1 {
+        &args[1]
+    } else {
+        return Err(anyhow!(
+            "Missing input file: please specify the path as argument"
+        ))?;
+    };
 
-    // Prepare csv writer and configure to write csv records to stdout
-    let mut csv_writer = csv::Writer::from_writer(io::stdout());
+    wrapper(csv_file)
+}
 
-    // Collect time-ordered transaction ids in transaction_history; transactions have to be processed in chronological order
-    let mut transaction_history: Vec<u32> = vec![];
-
-    // The transaction events (dispute, resolved, chargeback) are aggregated into the transactions so that transaction_id is unique in the input data
-    let mut transactions: HashMap<u32, Transaction> = HashMap::new();
-
-    // Prepare all transactions for processing (read from file, conversion to business objects)
-    for record in csv_reader.deserialize() {
-        let raw_transaction: RawTransaction = record?;
-
-        // Check and verify input format via type CheckedTransaction
-        let checked_transaction: CheckedTransaction = raw_transaction.try_into()?;
-
-        match checked_transaction {
-            CheckedTransaction::Transaction(tx) => match transactions.entry(tx.transaction_id) {
-                Entry::Occupied(_) => {
-                    return Err(FormatError::UniqueTransactionId(tx.transaction_id))?;
-                }
-                Entry::Vacant(entry) => {
-                    transaction_history.push(tx.transaction_id);
-                    entry.insert(tx);
-                }
-            },
-            CheckedTransaction::TransactionEvent(event) => {
-                match transactions.get_mut(&event.transaction_id) {
-                    Some(transaction) => {
-                        if transaction.client_id == event.client_id {
-                            transaction.events.push(event.event_type)
-                        } else {
-                            // Assumption: client_id and transaction_id of the transaction event have to coincide with the actual transaction; ignore if this is not the case
-                            continue;
-                        }
-                    }
-                    // Assumption: transaction events which do not reference a transaction id can be ignored
-                    None => continue,
-                }
-            }
-        };
-    }
+// Thin wrapper for testing
+fn wrapper(csv_file: &str) -> Result<(), anyhow::Error> {
+    // Read from file and convert RawTransactions to business objects
+    let (transaction_history, transactions) = csv::read(csv_file)?;
 
     // Process all transactions
     let accounts = payment_engine::process(&transaction_history, &transactions)?;
 
-    // Convert all business objects (Account) to the output format (RawAccount) and write to stdout in csv format
-    for (_client_id, account) in accounts {
-        let raw_account: RawAccount = account.into();
-        csv_writer.serialize(raw_account)?
-    }
+    // Convert business objects from Account to RawAccount and write to stdout in csv format
+    csv::write(accounts)?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_wrapper() {
+        let res = wrapper("test_data/transactions.csv");
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_wrapper_whitespaces() {
+        let res = wrapper("test_data/transactions_whitespaces.csv");
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_wrapper_invalid_type() {
+        let res = wrapper("test_data/transactions.csv");
+        assert!(res.is_ok());
+    }
+
+    #[test]
+    fn test_wrapper_invalid_transaction_type() {
+        let res = wrapper("test_data/transactions_invalid_transaction_type.csv");
+        assert!(res.is_err());
+
+        let err = res.unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            "Unexpected format: invalid transaction type 'unknown' in transaction id 5"
+        );
+    }
+
+    #[test]
+    fn test_wrapper_invalid_transaction_id() {
+        let res = wrapper("test_data/transactions_invalid_transaction_id.csv");
+        assert!(res.is_err());
+
+        let err = res.unwrap_err();
+        assert_eq!(
+            err.to_string(),
+            "Unexpected format: transaction id 1 is not unique"
+        );
+    }
 }
